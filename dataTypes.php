742644feba5cadf11a443c05b6de4c5eab705a16
Aminaa707 <mina.begum@moonabc.co.uk>
2022-02-04 21:57:35 +0600

    data types

---
+++ dataTypes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>8 types of Data Types have PHP</h4>
+    1. String
+    2. Integer
+    3. Float
+    4. Boolean
+    5. Array
+    6. Object 
+    7. NULL
+    8. Resource
+
+    <br/> <hr/>
+     <?php
+     
+     $null  ;
+     $learn = array("Ani", 10 , 10.5, false, null);
+     var_dump($learn)
+
+
+
+
+     ?>
+ 
+</body>
+</html>
---
+++ echo,print, variable.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello</h2>
+
+    <?php
+
+    echo "Echo is like print something. Echo's runtime  is firster then other print.Also we can pass double argument is echo.";
+  
+        echo  "<br>" ;
+   
+    print "Running my first program in PHP with XAMPP server";
+ 
+    
+    // variable
+
+   $first  = "Anika rrr";
+
+    echo $first;
+   echo  "<br>";
+   echo "<hr>";
+
+    // var_dump() is check data tipe
+    var_dump($first);
+    ?>
+</body>
+</html>
